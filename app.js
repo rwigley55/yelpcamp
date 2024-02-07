9ab59222dae0d0538605bb2c3401fd4623f73289
Reed Wigley <rwigley55@gmail.com>
2024-02-07 10:55:23 -0700

    Add seed functions and data

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
-app.get("/", (req, res) => {
-  res.send("Hello from Yelp Camp");
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
 });
 
+const app = express();
+
+//EJS
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+// ********************************************* //
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "Cheap camping!",
+  });
+  await camp.save();
+  res.send(camp);
+});
+
+// ********************************************* //
 app.listen(3000, () => {
   console.log("Serving on Port 3000");
 });
---
+++ seeds/index.js
@@ -0,0 +1,38 @@
+// To seed my DB
+// Call this index.js file in node
+// Deletes everything in the DB
+// Replaces/fills DB with new made-up campgrounds
+
+const mongoose = require("mongoose");
+const cities = require("./cities");
+const { places, descriptors } = require("./seedHelpers");
+const Campground = require("../models/campground");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+// Pass in an array, return random element from the array
+const sample = (array) => {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+const seedDB = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(descriptors)} ${sample(places)}`,
+    });
+    await camp.save();
+  }
+};
+
+seedDB().then(() => {
+  mongoose.connection.close();
+});
